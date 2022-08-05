e24a7bdcba4e7b2d2bb9a045c37b3b0b7f47ff1c
unknown <madhurigade50691@gmail.com>
2022-08-05 00:44:35 -0700

    student ServiceImpl class created

--- src/main/java/com/example/demo/Service/StudentServiceI.java
+++ src/main/java/com/example/demo/Service/StudentServiceI.java
@@ -1,5 +1,13 @@
 package com.example.demo.Service;
 
-public interface StudentServiceI {
+import java.util.List;
 
+import com.example.demo.model.Student;
+
+public interface StudentServiceI {
+ public Integer addStudent(Student student);
+ 
+ public List<Student>getAllStudent();
+ 
+ public Student getStudentById();
 }
--- src/main/java/com/example/demo/model/Student.java
+++ src/main/java/com/example/demo/model/Student.java
@@ -1,5 +1,43 @@
 package com.example.demo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Student {
+	@Id
+private int studentid;
+private String studentname;
+private String studentemail;
+private int studentage;
+public int getStudentid() {
+	return studentid;
+}
+public void setStudentid(int studentid) {
+	this.studentid = studentid;
+}
+public String getStudentname() {
+	return studentname;
+}
+public void setStudentname(String studentname) {
+	this.studentname = studentname;
+}
+public String getStudentemail() {
+	return studentemail;
+}
+public void setStudentemail(String studentemail) {
+	this.studentemail = studentemail;
+}
+public int getStudentage() {
+	return studentage;
+}
+public void setStudentage(int studentage) {
+	this.studentage = studentage;
+}
+@Override
+public String toString() {
+	return "Student [studentid=" + studentid + ", studentname=" + studentname + ", studentemail=" + studentemail
+			+ ", studentage=" + studentage + "]";
+}
 
 }
