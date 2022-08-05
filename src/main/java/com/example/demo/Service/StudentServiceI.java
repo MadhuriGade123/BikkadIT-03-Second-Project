08d3eddff9b4121dc3f06369dfe2df4864325632
unknown <madhurigade50691@gmail.com>
2022-08-05 02:10:50 -0700

    StudentRepository class created

--- src/main/java/com/example/demo/Service/StudentServiceI.java
+++ src/main/java/com/example/demo/Service/StudentServiceI.java
@@ -1,5 +1,12 @@
 package com.example.demo.Service;
 
+import java.util.List;
+
+import com.example.demo.model.Student;
+
 public interface StudentServiceI {
+	public int addStudent(Student student);
+	public List<Student>getAllStudent();
+	public Student getStudentById();
 
 }
