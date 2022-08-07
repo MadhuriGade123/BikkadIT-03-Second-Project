7e026b02dc74ebc1e7aad99b35e4113921aa24dd
unknown <madhurigade50691@gmail.com>
2022-08-07 03:08:16 -0700

    class added

--- src/main/java/com/example/demo/Service/StudentServiceI.java
+++ src/main/java/com/example/demo/Service/StudentServiceI.java
@@ -5,18 +5,18 @@ import java.util.List;
 import com.example.demo.model.Student;
 
 public interface StudentServiceI {
-<<<<<<< HEAD
- public Integer addStudent(Student student);
+
+ public Integer addStudent1(Student student);
  
- public List<Student>getAllStudent();
+ public List<Student>getAllStudent1();
  
  public Student getStudentById(Integer id);
 
 
-=======
+
 	public int addStudent(Student student);
 	public List<Student>getAllStudent();
 	public Student getStudentById();
->>>>>>> fbed30fc2e0f6d71f0cb556b4d979924e5924c70
+
 
 }
--- src/main/java/com/example/demo/Service/StudentServiceImpl.java
+++ src/main/java/com/example/demo/Service/StudentServiceImpl.java
@@ -1,51 +1,46 @@
 package com.example.demo.Service;
 
 import java.util.List;
-<<<<<<< HEAD
-=======
-
-import com.example.demo.model.Student;
-
-public class StudentServiceImpl implements StudentServiceI{
-
-	@Override
-	public int addStudent(Student student) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public List<Student> getAllStudent() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Student getStudentById() {
-		// TODO Auto-generated method stub
-		return null;
-	}
->>>>>>> fbed30fc2e0f6d71f0cb556b4d979924e5924c70
+	
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.example.demo.model.Student;
-import com.example.demo.repository.StudentRepository;
+
 
 public class StudentServiceImpl implements StudentServiceI {
 	@Autowired
 private StudentRepository studentRepository;
-	
+//	}
+//
+//	@Override
+//	public List<Student> getAllStudent() {
+//		List<Student> findAll = studentRepository.findAll();
+//		return findAll;
+//	}
+//
+//	@Override
+//	public Student getStudentById(Integer id) {
+//		Student student = studentRepository.findById(id).get();
+//		return student;
+//	}
+
 	@Override
-	public Integer addStudent(Student student) {
+	public Integer addStudent1(Student student) {
 		Student save = studentRepository.save(student);
 		return save.getStudentid();
 	}
 
 	@Override
-	public List<Student> getAllStudent() {
+	public List<Student> getAllStudent1() {
 		List<Student> findAll = studentRepository.findAll();
 		return findAll;
+	}
+
+	@Override
+	public int addStudent(Student student) {
+		Student save = studentRepository.save(student);
+		return save.getStudentid();
 	}
 
 	@Override
