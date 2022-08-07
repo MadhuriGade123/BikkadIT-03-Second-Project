f758c2fa00363fa358503d0ffbcc6f3bb76843fe
unknown <madhurigade50691@gmail.com>
2022-08-07 03:23:35 -0700

    employee added

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,5 +1,12 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EmployeeController {
+	
+	public void addEmployee() {
+		System.out.println("employee class added");
+	}
 
 }
